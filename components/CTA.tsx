f196fcba4443e2d521d3c1bc6ecf129a95ef9648
Sameem <ssssameembaba@gmail.com>
2025-03-13 04:33:17 +0530

    fix

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -13,9 +13,8 @@ const CTA = () => {
 
             {/* CTA Button */ }
             <Link
-                href="/contact"
+                href="/about"
                 className="text-white bg-gradient-to-r from-[#00c6ff] to-[#0072ff] hover:from-[#0096ff] hover:to-[#0057ff] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-md px-4 py-2 transition duration-300"
-                aria-label="Contact us"
             >
                 Click here
             </Link>
